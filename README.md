71078e53139f3bbc8a6add146647b8f72f45cb9a
James Duot <duot@users.noreply.github.com>
2024-01-23 22:09:12 -0800

    feat: count lines, and words

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # count
-Count file metrics: bytes
-
+Count file metrics: bytes (default is bytes), lines, words, chars.
 ## count bytes
 
 Counting all bytes is the default behavior. It's equivalent to running `count --type bytes`
+
+## todo
+
+- [ ] add test
+- [ ] extract functions to separate lib
+- [ ] support reading from std input
 
 ## License
 
--- main.go
+++ main.go
@@ -1,12 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"os"
 
 	"github.com/urfave/cli/v3"
 )
+
+func countBytes(fileName string) (int, error) {
+	bt, err := os.ReadFile(fileName)
+	if err != nil {
+		return 0, err
+	}
+	return len(bt), nil
+}
+
+func countLines(fileName string) (int, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanLines)
+
+	lines := 0
+	for s.Scan() {
+		lines++
+	}
+
+	return lines, nil
+}
+
+// countWords. Words are divided by consecutive spaces including newlines
+func countWords(fileName string) (int, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanWords)
+	lines := 0
+	for s.Scan() {
+		lines++
+	}
+	return lines, nil
+}
 
 func countCharacters(fileName string) (int, error) {
 	f, err := os.Open(fileName)
@@ -27,7 +71,7 @@ func countCharacters(fileName string) (int, error) {
 func main() {
 	cmd := &cli.Command{
 		Name:    "count all",
-		Usage: "Count file metric: bytes (default is bytes).",
+		Usage:   "Count file metric: bytes (default is bytes), lines, words, chars.",
 		Version: "v0.0.1",
 
 		Flags: []cli.Flag{
@@ -46,15 +90,50 @@ func main() {
 					return nil
 				},
 			},
-				Aliases: []string{"b"},
-				Name:    "bytes",
-				Value: true,
-				Usage:   "count bytes",
-			},
 			&cli.BoolFlag{
 				Aliases: []string{"w"},
-				Name:  "words",
-				Usage: "count words",
+				Name:    "words",
+				Value:   true,
+				Usage:   "count words",
+				Action: func(ctx context.Context, c *cli.Command, b bool) error {
+					fileName := c.Args().Get(0)
+					count, err := countWords(fileName)
+					if err != nil {
+						return err
+					}
+					fmt.Fprintln(os.Stdout, "\t", count, fileName)
+					return nil
+				},
+			},
+			&cli.BoolFlag{
+				Aliases: []string{"l"},
+				Name:    "lines",
+				Value:   true,
+				Usage:   "count lines",
+				Action: func(ctx context.Context, c *cli.Command, b bool) error {
+					fileName := c.Args().Get(0)
+					count, err := countLines(fileName)
+					if err != nil {
+						return err
+					}
+					fmt.Fprintln(os.Stdout, "\t", count, fileName)
+					return nil
+				},
+			},
+			&cli.BoolFlag{
+				Aliases: []string{"b"},
+				Name:    "bytes",
+				Value:   true,
+				Usage:   "count bytes",
+				Action: func(ctx context.Context, c *cli.Command, b bool) error {
+					fileName := c.Args().Get(0)
+					count, err := countBytes(fileName)
+					if err != nil {
+						return err
+					}
+					fmt.Fprintln(os.Stdout, "\t", count, fileName)
+					return nil
+				},
 			},
 		},
 		Action: func(_ context.Context, cmd *cli.Command) error {
